5061136696d625c521698d28cf72b4f7f359a7ac
mmrubayet <m.rubayet94@gmail.com>
2021-07-08 02:37:11 +0600

    modifying model

--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class State(models.Model):
     name    = models.CharField(max_length=100)
-    abbvr   = models.CharField(max_length=2)
+    abbvr   = models.CharField(max_length=2, unique=True)
     article = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-
+    
 class Faq(models.Model):
     question    = models.CharField(max_length=500)
     author      = models.ForeignKey(
