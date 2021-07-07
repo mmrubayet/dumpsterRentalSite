e4d72ff2f6973bca8f3fbf9c84b9de30fca88168
mmrubayet <m.rubayet94@gmail.com>
2021-07-07 22:14:05 +0600

    faqs adding functionality added using models and views

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Faq(models.Model):
+    question    = models.CharField(max_length=500)
+    author      = models.ForeignKey(
+            'auth.user',
+            on_delete=models.CASCADE,
+    )
+    answer =      models.TextField()
+
+    def __str__(self):
+        return self.question
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Faq
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -12,8 +14,10 @@ class AboutPageView(TemplateView):
 class ServicesPageView(TemplateView):
     template_name = 'services.html'
 
-class FAQPageView(TemplateView):
+class FAQPageView(ListView):
+    model = Faq
     template_name = 'faqs.html'
+    context_object_name = 'all_faqs_list'
 
 class ContactPageView(TemplateView):
     template_name = 'contact.html'
