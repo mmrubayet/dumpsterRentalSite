945bfde162366d6fbd47fafa42eb72e806f434ff
mmrubayet <m.rubayet94@gmail.com>
2021-07-11 07:04:59 +0600

    text editor added in state and city model

--- state/models.py
+++ state/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
+
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
 class State(models.Model):
     name        = models.CharField(max_length=100, unique=True)
     abbvr       = models.CharField(max_length=2, unique=True)
-    article     = models.TextField(null=False)
-    slug   = models.SlugField(null=False, unique=True)
+    # article     = models.TextField(null=False)
+    article     = RichTextField(blank=True, null=True)
+    slug        = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.name
@@ -26,8 +29,9 @@ class City(models.Model):
     state_name  = models.ForeignKey('state.State', on_delete=models.CASCADE, related_name='cities')
     city_name   = models.CharField(max_length=50)
     zip         = models.CharField(max_length=10, )
-    city_detail = models.TextField(null=False)
-    slug   = models.SlugField(null=False, unique=True)
+    # city_detail = models.TextField(null=False)
+    city_detail = RichTextField(blank=True, null=True)
+    slug        = models.SlugField(null=False, unique=True)
 
     def __str__(self):
         return self.city_name
