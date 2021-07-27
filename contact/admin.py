b98f90b3472cd33a9f662389dbc4c63252fae17b
mmrubayet <m.rubayet94@gmail.com>
2021-07-27 06:39:15 +0600

    contact view updated on admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'location', 'message')
+
+admin.site.register(Contact, ContactAdmin)
