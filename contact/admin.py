aa7e8ae995179cb769ec09ff3894d614ca18ae08
mmrubayet <m.rubayet94@gmail.com>
2021-07-27 09:17:40 +0600

    added timestamp on contact model

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'location', 'message')
+    list_display = ('name', 'email', 'phone', 'location', 'message', 'received')
 
 admin.site.register(Contact, ContactAdmin)
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Contact(models.Model):
@@ -8,6 +8,7 @@ class Contact(models.Model):
     phone       = models.CharField(max_length=255)
     location    = models.CharField(max_length=255)
     message     = models.TextField()
+    received    = models.DateTimeField(auto_now_add=True) #default= timezone.now
 
     def __str__(self):
         return self.name
