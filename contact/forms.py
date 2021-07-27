53280ad35cad002f99d3666768c0b97d7f637e58
mmrubayet <m.rubayet94@gmail.com>
2021-07-27 06:05:22 +0600

    beautified contact form

--- contact/forms.py
+++ contact/forms.py
@@ -1,8 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Contact
 
 
-class ContactForm(ModelForm):
+class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'contactus', 'placeholder':'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'contactus', 'placeholder':'Email'}),
+            'phone': forms.TextInput(attrs={'class': 'contactus', 'placeholder':'Phone Number'}),
+            'location': forms.TextInput(attrs={'class': 'contactus', 'placeholder':'Desired Service Area'}),
+            'message': forms.Textarea(attrs={'class': 'contactus', 'placeholder':'Message'}),
+        }
